7a85c9b5a53a46450b118f0adcadc5442718c866
priyankakankal <priyankakankal17@google.com>
2020-02-17 15:25:29 +0530

    Flood Fill Algorithm

---
+++ myflood8.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <graphics.h>
+
+void myflood(int x, int y) {
+	int current;
+	current = getpixel(x, y);
+	if(current == 0) {
+		putpixel (x, y, 10);
+		myflood(x + 1, y);
+		myflood(x - 1, y);
+		myflood(x, y + 1);
+		myflood(x, y - 1);
+		myflood(x + 1, y - 1);
+		myflood(x - 1, y + 1);
+		myflood(x + 1, y + 1);
+		myflood(x - 1, y - 1);
+	}
+}
+
+int main() {
+	int gd = DETECT,gm;
+	int x, y, current;
+	int left = 150, top = 150;
+	int right = 450, bottom = 450;
+	initgraph(&gd,&gm,NULL);
+	rectangle(left, top, right, bottom);
+	x = (left + right)/2;
+	y = (top + bottom)/2;
+	for(; x <= right; x++) {
+		for(; y <= bottom; y++) {
+			myflood(x, y);
+		}
+	}
+	delay(1000);
+	closegraph();
+	return 0;
+}
+
+
